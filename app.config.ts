4512bb13358afa1067c005fbfb746ff816820f73
BrunoCaputo <brunopc.dev@gmail.com>
2025-07-02 21:14:23 -0300

    chore: Update app config

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,41 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   expo: {
-    name: 'Ignite Shoes App',
-    slug: 'igniteshoesapp',
-    version: '1.0.0',
+    name: "Ignite Shoes App",
+    slug: "igniteshoesapp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#121214",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.igniteshoesapp",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#121214",
+      },
+      package: "com.anonymous.igniteshoesapp",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      "expo-font",
+      [
+        "onesignal-expo-plugin",
+        {
+          mode: "development",
+        },
+      ],
+    ],
     extra: {
       oneSignalAppId: process.env.ONE_SIGNAL_APP_ID,
     },
