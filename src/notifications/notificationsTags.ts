27dbe33ac6db981451ed865b4f5f085c3d6e07e2
BrunoCaputo <brunopc.dev@gmail.com>
2025-07-14 23:32:30 -0300

    feat: Handle notification tags

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -3,3 +3,11 @@ import { OneSignal } from "react-native-onesignal";
 export function createTagUserEmail(email: string) {
   OneSignal.User.addTag("user_email", email);
 }
+
+export function removeTagUserEmail() {
+  OneSignal.User.removeTag("user_email")
+}
+
+export function createTagUserInfo() {
+  OneSignal.User.addTags({})
+}
