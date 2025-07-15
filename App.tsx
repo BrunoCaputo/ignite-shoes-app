9765d19abbe630b0ec3d3de6aa0185750e3bf49e
BrunoCaputo <brunopc.dev@gmail.com>
2025-07-14 23:55:18 -0300

    feat: Handle background notification click

--- App.tsx
+++ App.tsx
@@ -1,25 +1,57 @@
-import { Platform, StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from 'react-native-onesignal';
+import { Platform, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal";
 
-import { env } from './src/env/env';
-import { Routes } from './src/routes';
+import { env } from "./src/env/env";
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
-import { createTagUserEmail } from './src/notifications/notificationsTags';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { createTagUserEmail } from "./src/notifications/notificationsTags";
+import { useEffect } from "react";
 
-const oneSignalAppId = Platform.OS === "ios" ? env.oneSignalIosAppId : env.oneSignalAndroidAppId;
-OneSignal.initialize(oneSignalAppId)
-OneSignal.Notifications.requestPermission(true)
+const oneSignalAppId =
+  Platform.OS === "ios" ? env.oneSignalIosAppId : env.oneSignalAndroidAppId;
+OneSignal.initialize(oneSignalAppId);
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  createTagUserEmail('test@email.com')
+  createTagUserEmail("test@email.com");
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {
+      const { actionId } = event.result;
+
+      switch (actionId) {
+        case "1":
+          console.log("Action 1");
+          break;
+        case "2":
+          console.log("Action 2");
+          break;
+        default:
+          console.log("None");
+          break;
+      }
+    };
+
+    OneSignal.Notifications.addEventListener("click", handleNotificationClick);
+
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        "click",
+        handleNotificationClick
+      );
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
